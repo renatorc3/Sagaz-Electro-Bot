90959fce9b892c2088f58d1e1e49fd37f308eaaa
NiceATC <NiceATC@users.noreply.github.com>
2017-07-14 02:54:30 -0300

    Update bBot.js

--- bBot.js
+++ bBot.js
@@ -46,7 +46,7 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "Loli Bot",
+      botName: "Sagaz Bot",
       language: "portuguese",
       chatLink: "https://rawgit.com/renatorc3/Sagaz-Electro-Bot/master/pt-BR.json",
       scriptLink: "https://rawgit.com/renatorc3/Sagaz-Electro-Bot/master/basicBot.js",
